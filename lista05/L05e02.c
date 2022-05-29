0c838880b340662357ad9ad5d77deef4619acf77
fgiongo <fernandogiongooliveira@gmail.com>
2022-05-28 22:24:39 -0300

    adicionando ex02 da lista 5

---
+++ lista05/L05e02.c
@@ -0,0 +1,47 @@
+/* Nome do arquivo: L05e02.c
+ * Data: 28/05/2022
+ * Autor: Fernando Giongo
+ * Descricao: Este programa le duas matrizes 2 x 3 de numeros inteiros e
+ * imprime a sua soma.
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int matriz[3][2][3], i, j, k;
+
+    for (i = 0; i < 2; i++)
+    {
+        printf("Matriz %d (2 x 3):\n", i + 1);
+        for (j = 0; j < 2; j++)
+        {
+            printf("Inserir 3 valores(separados por espaco) para linha %d: ",
+                    j + 1);
+            scanf("%d %d %d", &matriz[i][j][0], &matriz[i][j][1],
+                    &matriz[i][j][2]);
+        }
+    }
+
+    /* removendo o lixo */
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+            for (k = 0; k < 3; k++)
+                matriz[2][j][k] = 0;
+    
+    /* executando a soma */
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+            for (k = 0; k < 3; k++)
+                matriz[2][j][k] += matriz[i][j][k];
+
+    printf("\nMatriz soma de Matriz 1 e Matriz 2\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+            printf("%03d ", matriz[2][i][j]);
+
+        printf("\n");
+    }
+    return 0;
+}
