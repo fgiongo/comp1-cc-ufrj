af52d13135302f6f616da545a128c2d8aed8cb5a
fgiongo <fernandogiongooliveira@gmail.com>
2022-05-31 21:37:03 -0300

    terminando ex06 da lista 05

--- lista05/L05e06.c
+++ lista05/L05e06.c
@@ -15,7 +15,7 @@
  * 	(b) calcula a nota final como a soma da media das provas e da media
  * 	    dos trabalhos
  * 	(c) imprime a matricula do aluno que obteve a maior nota final
- * 	    (assumendo que so existe uma maior nota)
+ * 	    (assumindo que so existe uma maior nota)
  * 	(d) imprime a media aritmetica das notas finais
  */
 
@@ -26,12 +26,23 @@
 
 void matrix_input_notas(int line, int col, int *matrix);
 void input_flush(void);
+void calcular_media(int lines, int cols, int *matrix);
+void matrix_print(int line, int col, int *matrix);
+int matricula_maior_nota_final(int lines, int cols, int *matrix);
+float media_notas_finais(int lines, int cols, int *matrix);
 
 int main (void)
 {
     int matriz[LINES][COLUMNS];
 
     matrix_input_notas(LINES, COLUMNS, &matriz[0][0]);
+    calcular_media(LINES, COLUMNS, &matriz[0][0]);
+    printf("\n");
+    matrix_print(LINES, COLUMNS, &matriz[0][0]);
+    printf("\nMatricula do aluno com maior nota final: %d\n",
+            matricula_maior_nota_final(LINES, COLUMNS, &matriz[0][0]));
+    printf("\nMedia aritmetica das notas finais: %.2f\n",
+            media_notas_finais(LINES, COLUMNS, &matriz[0][0]));
     return 0;
 }
 
@@ -65,16 +76,96 @@ void input_flush(void)
 
 void calcular_media(int lines, int cols, int *matrix)
 {
-    int i, j, *final;
+    int i, j, *final_ptr;
 
     for (i = 0; i < lines; i++)
     {
         final_ptr = matrix + i * cols + (cols - 1);
+        *final_ptr = 0;
 
-        for (int j = 0; j < cols; j++)
+        for (j = 0; j < cols; j++)
         {
             if (j == 0 || j == cols - 1) continue;
             *final_ptr += *(matrix + i * cols + j);
         }
     }
 }
+
+void matrix_print(int line, int col, int *matrix)
+{
+    int i, j;
+    for (i = 0; i < line; i++)
+    {
+        for (j = 0; j < col; j++)
+            printf("%8d ", *(matrix + i * col + j)); 
+
+        printf("\n");
+    }
+}
+
+int matricula_maior_nota_final(int lines, int cols, int *matrix)
+{
+    int i, final, matricula, *matricula_ptr, *final_ptr;
+    final = 0;
+
+    for (i = 0; i < lines; i++)
+    {
+        matricula_ptr = matrix + i * cols;
+        final_ptr = matrix + i * cols + (cols - 1);
+        if (*final_ptr > final)
+        {
+            final = *final_ptr;
+            matricula = *matricula_ptr;
+        }
+    }
+    return matricula;
+}
+
+float media_notas_finais(int lines, int cols, int *matrix)
+{
+    int i, final, *final_ptr;
+    final = 0;
+
+    for (i = 0; i < lines; i++)
+    {
+        final_ptr = matrix + i * cols + (cols - 1);
+        final += *final_ptr;
+    }
+
+    return final / (float) lines;
+}
+
+/* Saidas
+
+Entrada de dados:
+(1) Numero de matricula
+(2) Media das provas
+(3) Media dos trabalhos
+
+Inserir os 3 valores(separados por espaco) para aluno no. 1:
+101 23 48
+
+Inserir os 3 valores(separados por espaco) para aluno no. 2:
+102 39 42
+
+Inserir os 3 valores(separados por espaco) para aluno no. 3:
+103 18 33
+
+Inserir os 3 valores(separados por espaco) para aluno no. 4:
+104 49 41
+
+Inserir os 3 valores(separados por espaco) para aluno no. 5:
+105 26 30
+
+     101       23       48       71 
+     102       39       42       81 
+     103       18       33       51 
+     104       49       41       90 
+     105       26       30       56 
+
+Matricula do aluno com maior nota final: 104
+
+Media aritmetica das notas finais: 69.80
+
+*/
+
