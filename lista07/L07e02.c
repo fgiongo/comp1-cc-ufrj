4ed8edde0e1cff94a7d4c59d5fe1a701763c5b19
fgiongo <fgiongo@protonmail.com>
2022-06-16 12:40:38 -0300

    criando diretorios e fontes

---
+++ lista07/L07e02.c
@@ -0,0 +1,17 @@
+/*
+ * 	Programa: nomeDoPrograma.c
+ * 	Autor: Fernando Giongo
+ * 	Data: dd/mm/aaaa
+ * 	Descrição:
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	/* Aqui vao as declaracoes das variaveis */
+
+	/* Aqui vao os comandos do programa */
+
+	return 0;
+}
