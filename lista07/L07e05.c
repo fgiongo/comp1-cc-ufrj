8ae23ab6aad96e3d4400ad05d23c1e6b276a09f2
fgiongo <fgiongo@protonmail.com>
2022-06-16 13:58:25 -0300

    L07e05 pronto

--- lista07/L07e05.c
+++ lista07/L07e05.c
@@ -1,17 +1,54 @@
 /*
- * 	Programa: nomeDoPrograma.c
+ * 	Programa: L07e05
  * 	Autor: Fernando Giongo
- * 	Data: dd/mm/aaaa
+ * 	Data: 16/06/2022
  * 	Descrição:
+ *
+ * 	O programa pega uma cadeia e retorna a segunda palavra desta cadeia.
+ * 	Isso e' feito por meio de uma funcao que percorre a cadeia e retorna um
+ * 	ponteiro para o endereco apos o primeiro espaco.
+ *
+ * 	O programa entao imprime a cadeia, usando como endereco de referencia o
+ * 	retorno da funcao. Desta forma, o programa imprime a cadeia a partir da
+ * 	segunda palavra.
  */
 
 #include <stdio.h>
 
+char * acheSobrenome(char nome[])
+{
+    char *pnome;
+    int i = 0;
+
+    while (nome[i] != ' ')
+    {
+        i++;
+    }
+    i++;
+    pnome = &nome[i];
+    return pnome;
+}
+
 int main (void)
 {
-	/* Aqui vao as declaracoes das variaveis */
+    char nomeCompleto[80];
+    char *p;
 
-	/* Aqui vao os comandos do programa */
+    puts("Entre com o seu nome e um sobrenome.");
+    gets(nomeCompleto);
+
+    p = acheSobrenome(nomeCompleto);
+
+    puts(p);
 
 	return 0;
 }
+
+/* Saida
+
+Entre com o seu nome e um sobrenome.
+Fernando Giongo
+Giongo
+
+*/
+
